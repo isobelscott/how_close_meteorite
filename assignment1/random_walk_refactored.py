f5e9b9299d4e7344ceb625b158f3d8fb2e724d6e
isobelsv <isobelascott@gmail.com>
2017-07-20 15:23:30 -0400

    added random_walk visualisation

--- assignment1/random_walk_refactored.py
+++ assignment1/random_walk_refactored.py
@@ -3,6 +3,9 @@
 import random as rd
 import numpy as np
 import matplotlib.pyplot as plt
+import math
+
+""" Take a random walk. """
 
 def walk(steps):
     step_list = [list((rd.randint(-1, 1), rd.randint(-1, 1)))]
@@ -10,12 +13,74 @@ def walk(steps):
         step_list.append(list((rd.randint(-1, 1), rd.randint(-1, 1))))
     return step_list
 
+""" Calculate distance journeyed with the walk. """
+
+def distance(walk):
+    total_distance = 0
+    for coords in walk:
+        dx = coords[0]
+        dy = coords[1]
+
+        distance = (math.sqrt(dx*dx + dy*dy)) * 2.58 # the distance in steps multiplied by the average number of feet in a step
+        total_distance += distance
+
+    return total_distance
+
+""" Take many walks. """
+
+def many_walks(walks):
+    walk_list = []
+    while (len(walk_list)) < walks:
+         walk_list.append(walk(steps))
+    return walk_list
+
+""" Get the average distance of a walk, when the walker has taken many walks. """
+
+def average_distance(walk_list, walks):
+    total_distance_all = 0
+
+    for stroll in walk_list:
+        for coords in stroll:
+            dx = coords[0]
+            dy = coords[1]
+        distance = (math.sqrt(dx*dx + dy*dy)) * 2.58 # the distance in steps multiplied by the average number of feet in a step
+        total_distance_all += distance
+
+    return total_distance_all
+    average_dist = (total_distance_all / walks)
+    return average_dist
+
+
+
 if __name__ == "__main__":
     steps = 8
     walk1 = np.array(walk(steps))
+    print('During this walk, the walker moved as follows: ')
     print(walk1)
-    plt.plot(walk1)
-    plt.title('Random Walk')
+
+    distance = distance(walk1)
+    distance1 = round(distance, 2)
+    print('The distance journeyed on this walk is: ')
+    print(str(distance1) + ' feet')
+
+    walks = 4
+    walk_list1 = many_walks(walks)
+    print(walk_list1)
+
+    avg_dist_all_walks = average_distance(walk_list1, walks)
+    avg_dist_all_walks1 = round(avg_dist_all_walks, 2)
+    print('The average distance journeyed on these walks is: ')
+    print(str(avg_dist_all_walks1) + ' feet')
+
+
+    """ Visualize the random walks."""
+
+    print('The walks look like this: ')
+    walk_list2 = np.vstack(walk_list1)
+    plt.plot(walk_list2)
+    plt.title('Random Walks')
     plt.ylabel('Moves North or South')
     plt.xlabel('Moves East or West')
-    plt.show()
+    plt.show(block=True)
+
+
