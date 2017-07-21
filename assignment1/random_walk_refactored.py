2bd4ec301af3bd44ca13923b05f197a572332b42
isobelsv <isobelascott@gmail.com>
2017-07-21 15:00:21 -0400

    moved doc strings to right spot

--- assignment1/random_walk_refactored.py
+++ assignment1/random_walk_refactored.py
@@ -5,17 +5,17 @@ import numpy as np
 import matplotlib.pyplot as plt
 import math
 
-""" Take a random walk. """
 
 def walk(steps):
+    """ Take a random walk. """
     step_list = [list((rd.randint(-1, 1), rd.randint(-1, 1)))]
     while (len(step_list)) < steps:
         step_list.append(list((rd.randint(-1, 1), rd.randint(-1, 1))))
     return step_list
 
-""" Calculate distance journeyed with the walk. """
 
 def distance(walk):
+    """ Calculate distance journeyed with the walk. """
     total_distance = 0
     for coords in walk:
         dx = coords[0]
@@ -26,17 +26,17 @@ def distance(walk):
 
     return total_distance
 
-""" Take many walks. """
 
 def many_walks(walks):
+    """ Take many random walks. """
     walk_list = []
     while (len(walk_list)) < walks:
          walk_list.append(walk(steps))
     return walk_list
 
-""" Get the average distance of a walk, when the walker has taken many walks. """
 
 def average_distance(walk_list, walks):
+    """ Get the average distance of a walk, when the walker has taken many walks. """
     total_distance_all = 0
 
     for stroll in walk_list:
@@ -49,7 +49,6 @@ def average_distance(walk_list, walks):
     return total_distance_all
     average_dist = (total_distance_all / walks)
     return average_dist
-
 
 
 if __name__ == "__main__":
@@ -73,8 +72,8 @@ if __name__ == "__main__":
     print(str(avg_dist_all_walks1) + ' feet')
 
 
-    """ Visualize the random walks."""
-
+    # Visualize the random walks.
+    
     print('The walks look like this: ')
     walk_list2 = np.vstack(walk_list1)
     plt.plot(walk_list2)
