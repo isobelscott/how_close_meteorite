1f344b9ae754dee8d4ea3d72daf9187ecd9d2261
isobelsv <isobelascott@gmail.com>
2017-07-24 16:48:26 -0400

    able to deal game now

--- assignment2/poker.py
+++ assignment2/poker.py
@@ -1,19 +1,20 @@
 #!/usr/bin/env python
 
+import random
 
 class Card(object):
     """ represents a standard playing card. """
-
+    
     def __init__(self, suit=0, rank=2):
         self.suit = suit
         self.rank = rank
 
 
     suit_names = ["Clubs", "Diamonds", "Hearts", "Spades"]
-    rank_names = [None, "Ace", "2", "3", "4", "5", "6", "7",
+    rank_names = [None,"Ace", "2", "3", "4", "5", "6", "7",
                "8", "9", "10", "Jack", "Queen", "King"]
 
-    def __str__(self):
+    def __repr__(self):
         return "%s of %s" % (Card.rank_names[self.rank],
                              Card.suit_names[self.suit])
 
@@ -24,9 +25,8 @@ class Card(object):
         return cmp(t1, t2)
 
 
+class Deck(Card):
 
-class Deck(object):
-    
     def __init__(self):
         self.cards = []
         for suit in range(4):
@@ -46,14 +46,42 @@ class Deck(object):
     def top_card(self):
         return self.cards[0]
 
+    def add_card(self, card):
+        self.cards.append(card) 
+
+    def shuffle(self):
+        random.shuffle(self.cards)
+
+    def sort(self):
+        __cmp__.sort(self.cards)
+
+    def move_cards(self, hand, num):
+        for i in range(num):
+            hand.add_card(self.pop_card())
+   
+    def deal_hand(self, card_num):
+        hand1 = []
+        while len(hand1) < card_num:
+            card1 = self.pop_card()
+            hand1.append(card1)
+        return hand1
+
+    def deal_game(self, player_num, card_num):
+        game1 = []
+        while len(game1) < player_num:
+            hand1 = self.deal_hand(card_num)
+            game1.append(hand1)
+        return game1
 
 if __name__ == "__main__":
-    card1 = Card(2, 11)
-    print(card1)
-    deck1 = Deck()
-    print(deck1)
-    bot = deck1.pop_card()
-    top = deck1.top_card()
-    print(bot)
-    print(top)
+    
+    deck = Deck()
+    deck.shuffle()
+    game = deck.deal_game(3, 5)
+    print(game)
+
+
+
+
+
 
