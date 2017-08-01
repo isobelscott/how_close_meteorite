3e6b654d206dd96fe3a47b42aabc81d92613459f
isobelsv <isobelascott@gmail.com>
2017-08-01 14:54:24 -0400

    3 tests on Shape Class of flatland

--- assignment3/ShapeTest.py
+++ assignment3/ShapeTest.py
@@ -1,22 +1,35 @@
 #!/usr/bin/env python3
 
 import unittest
-from Shape import Shape
-
+from flatland import Shape
+from random import randint
 
 class TestShape(unittest.TestCase):
-    """ Tests the Shape class in flatlands. """
-    def setUp(self):
+    """ Tests the methods in the Shape class in flatland. """
+    
+    #  test the staticmethod to show list as individual strings
+    def test_ea_list(self):
         self.myShape = Shape(4)
         self.ealist_template = 'a, b, c, d'
         list1 = list(['a','b','c','d'])
-        print(list1)
         self.ealist_test = self.myShape.ea_list(list1)
-        print(self.ealist_test)
-
-    def test_ea_list(self):
         self.assertEqual(self.ealist_template,self.ealist_test)
 
+    # test the side length update
+    def test_update_side_length(self):
+        self.side = randint(1, 50)
+        self.myShape = Shape(self.side)
+        self.change = randint(-20, 20)
+        self.side = self.side + self.change
+        self.assertTrue(Shape(self.side))
+
+    # test that the ally has been added to the ally list
+    allies = []
+    def test_add_ally(self):
+        self.shape_object = 'a'
+        allies = TestShape.allies
+        allies.append(self.shape_object)
+        self.assertEqual(allies, list(self.shape_object))
 
 if __name__ == '__main__':
     unittest.main()
