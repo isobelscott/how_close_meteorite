a6a667a24586551ab995c7ff2dc70a2e78918698
isobelsv <isobelascott@gmail.com>
2017-08-06 12:40:39 -0400

    pandas y'all

---
+++ getdata.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+
+import csv
+import pandas as pd
+import numpy as np
+
+
+with open('/Users/isobel/Downloads/asteroid-impacts/impacts.csv') as csvfile:
+    impactreader = csv.reader(csvfile, delimiter=',', quotechar='|')
+    impact_name_list = []
+    diameter_list = []
+    for row in impactreader:
+        diameter_list.append(row[7])
+        impact_name = row[0].split(" ")
+        if impact_name[-1][-1] == ')':
+            impact_name[-1] = impact_name[-1][:-1]
+        impact_name_list.append(impact_name[-1])
+    a = np.column_stack((impact_name_list, diameter_list))
+    #print(a)
+
+
+
+with open('/Users/isobel/Downloads/asteroid-impacts/orbits.csv') as csvfile:
+    orbitreader = csv.reader(csvfile, delimiter=',', quotechar='|')
+    orbit_name_list = []
+    orbit_peri_list = []
+    for row in orbitreader:
+        orbit_name = row[0].split("\xa0")
+        orbit_name = orbit_name[-1][:-1]
+        if orbit_name == "Object Nam": 
+            orbit_name = "Name"
+        orbit_name_list.append(orbit_name)
+        peri_dist = row[9]
+        orbit_peri_list.append(peri_dist)
+    b = np.column_stack((orbit_name_list, orbit_peri_list))
+    #print(b)
+
+c = []
+for row in a:
+    index = np.where(b[:,0] == row[0])[0]
+    if np.size(index) !=0:
+        c.append([row[0], row[1], b[index[0], 1]])
+    else:
+        c.append([row[0], row[1], 0])
+
+d = pd.DataFrame(c)
+print(d)
+        
