f2da23c0c69b3dce6adc240bb92ab1ab221f6482
isobelsv <isobelascott@gmail.com>
2017-08-13 13:44:38 -0400

    added data that makes csv json & yaml

--- assignment4/ioassignment.py
+++ assignment4/ioassignment.py
@@ -3,21 +3,21 @@
 import csv
 import json 
 import yaml
+import tablib
 
-
-def make_json(file_name_csv):
-    file_name_json = 'input.json'
+def csv_to_json(csv_file):
+    json_file = 'input.json'
     data = dict() 
 
-    with open(file_name_csv, 'r') as csvfile:
+    with open(csv_file, 'r') as csvfile:
         reader = csv.DictReader(csvfile)
         rows = list(reader)
 
-    with open(file_name_json, 'w') as jsonfile:
+    with open(json_file, 'w') as jsonfile:
         json.dump(rows, jsonfile)
 
-def make_yaml(file_name_csv):
-    with open(file_name_csv, 'r') as csvfile:
+def csv_to_yaml(csv_file):
+    with open(csv_file, 'r') as csvfile:
         next(csvfile)
         freader = csv.reader(csvfile, delimiter=' ', quotechar='|')
         yaml_file = open('input.yaml', 'w')
@@ -32,8 +32,31 @@ def make_yaml(file_name_csv):
             yaml_file.write('   fcolor: %s\n' % csv_content[4])
             yaml_file.write('   fmovie: %s\n' % csv_content[5])
         yaml_file.close()
-        
+
+def data():
+    d = tablib.Dataset()
+    d.append_col(['theo', 'simon', 'roger', 'jasper'])
+    d.append_col(['student', 'statistician', 'analyst', 'baker'])
+    d.headers = ['fname', 'occupation']
+    return d
+
+def make_csv(data):
+    with open('output.csv', 'w') as newcsv:
+        newcsv.write(data.csv)
+
+def make_json(data):
+    with open('output.json', 'w') as newjson:
+        newjson.write(data.json)
+
+def make_yaml(data):
+    with open('output.yaml', 'w') as newyaml:
+        newyaml.write(data.yaml)
+
 if __name__ == '__main__':
-    file_name_csv = 'input.csv'
-    #make_json(file_name_csv)
-    make_yaml(file_name_csv)
+    csv_file = 'input.csv'
+    csv_to_json(csv_file)
+    csv_to_yaml(csv_file)
+    data = data()
+    make_csv(data)
+    make_json(data)
+    make_yaml(data)
